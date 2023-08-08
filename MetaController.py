e17058c6eb80cf1561d74ade85b7e0c3fdcffb73
hamed-karimi <hamedk72@yahoo.com>
2023-08-08 13:12:33 -0400

    no mask on DQN output

--- MetaController.py
+++ MetaController.py
@@ -102,7 +102,7 @@ class MetaController:
         self.memory.push_experience(initial_map, initial_need, goal_map, acquired_reward, done, final_map, final_need)
         # relu = nn.ReLU()
         sigmoid = nn.Sigmoid()
-        memory_prob = sigmoid(acquired_reward) + .05
+        memory_prob = sigmoid(acquired_reward) + .1
         # memory_prob = relu(acquired_reward) + 1  # This should be changed to sigmoid
         # memory_prob = .5 if acquired_reward == 0 else 1/abs(acquired_reward)
         # memory_prob = 1
