0bb74caa9a159a39baf54a5c339f3f7924bc6ee7
hamed-karimi <hamedk72@yahoo.com>
2023-09-28 16:26:20 -0400

    discounting or rewards and Q happens in MetaController

--- MetaController.py
+++ MetaController.py
@@ -73,7 +73,7 @@ class MetaController:
         self.epsilon_list = []
 
     def gamma_function(self, episode):
-        m = 4
+        m = 3
         ratio = m / self.target_net_update
         gamma = min(1 / (1 + math.exp(-episode * ratio + math.exp(2.3))),
                     self.max_gamma)
